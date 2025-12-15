abb91fc3e9a8868b07cb3fe3c67c3970a2947f98
yong <den9yon9@gmail.com>
2025-12-15 09:36:37 +0800

    fix: vscode settings

--- apps/www/src/pages/home/components/Home.tsx
+++
@@ -1,7 +0,0 @@
-import { query } from "@/lib/api";
-
-export default function Home() {
-  const list = query.get.useSuspenseQuery();
-
-  return <div>{JSON.stringify(list.hello)}</div>;
-}
--- apps/www/src/pages/home/index.tsx
+++ apps/www/src/pages/home/index.tsx
@@ -1,1 +1,6 @@
-export { default } from "./components/Home";
+import { query } from "../../lib/api";
+
+export default function Home() {
+  const mine = query.user.mine.get.useSuspenseQuery();
+  return <div>{JSON.stringify(mine)}</div>;
+}
